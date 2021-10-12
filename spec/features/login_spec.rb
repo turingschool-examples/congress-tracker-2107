e52df1cf6efee2ab8b4c646d7ff35c9e9204263a
Meg Stang <mstang15@gmail.com>
2021-10-12 11:57:01 -0600

    Authorization

---
+++ spec/features/login_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+RSpec.describe "Logging In" do
+  it "can log in with valid credentials" do
+    user = User.create(username: "funbucket13", password: "test")
+
+    visit root_path
+
+    click_on "I already have an account"
+
+    expect(current_path).to eq(login_path)
+
+    fill_in :username, with: user.username
+    fill_in :password, with: user.password
+
+    click_on "Log In"
+
+    expect(current_path).to eq(root_path)
+
+    expect(page).to have_content("Welcome, #{user.username}")
+    expect(page).to_not have_link("Register as a User")
+    expect(page).to_not have_link("I already have an account")
+    expect(page).to have_link("Log out")
+  end
+
+  it "cannot log in with bad credentials" do
+    user = User.create(username: "funbucket13", password: "test")
+  
+    # we don't have to go through root_path and click the "I have an account" link any more
+    visit login_path
+  
+    fill_in :username, with: user.username
+    fill_in :password, with: "incorrect password"
+  
+    click_on "Log In"
+  
+    expect(current_path).to eq(login_path)
+  
+    expect(page).to have_content("Sorry, your credentials are bad.")
+  end
+end
---
+++ spec/features/register_user_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe "User registration form" do
+  it "creates new user" do
+    visit root_path
+
+    click_on "Register as a User"
+
+    expect(current_path).to eq(new_users_path)
+
+    username = "funbucket13"
+    password = "test"
+
+    fill_in :user_username, with: username
+    fill_in :user_password, with: password
+
+    click_on "Create User"
+
+    new_user = User.last
+
+    expect(page).to have_content("Welcome, #{username}!")
+    expect(new_user.username).to eq(username)
+    expect(new_user.password_digest).to_not eq(password)
+  end
+end
